35c85ea3555c94830d451405b0e2f72f7495b4d0
jhourodriguez5701 <jhousaso@gmail.com>
2019-11-14 09:42:16 -0500

    Agregar archivos de proyecto.

---
+++ AppProgramacionCaps/Factorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppProgramacionCaps
+{
+    class Factorial
+    {
+        public static long fact (int num)
+        {
+            long f = 1;
+            if (num == 0 || num == 1)
+                return f;
+            for (int i = 1; i <= num; i++)
+                f = f * i;
+            return f;
+        }
+    }
+}
